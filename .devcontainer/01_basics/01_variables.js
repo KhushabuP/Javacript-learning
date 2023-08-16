5005a0fecaa1f11dda2a3a7db5c3e1bd7c749136
KhushabuP <142418862+KhushabuP@users.noreply.github.com>
2023-08-16 20:09:48 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 12345
+let accountEmail = "kp@gmail.com"
+var accountPassword ="1232123"
+accountcity = "Pune"
+
+console.log(accountId);
+
+/*
+Prefer not use "var"
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail,accountPassword, accountCity])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //  we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    console.log("Khushabu")
+
+    let name = "khushi"
+    let age = 24
+    let isLoggedIn = false
+
+
+    //Data Types
+
+    // number ==> 2 to power 53
+    // bigint
+    // strings ==> ""
+    //boolean ==> true/false
+    //null ==> standalone value
+    //undefined ==>not defined any value
+    //symbol ==> to define the uniqueness
+
+    //Objects
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
+    
+
+
+    
