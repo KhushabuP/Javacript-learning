987b3b472af9574d5c02b4dd2d2674f329d2172d
KhushabuP <142418862+KhushabuP@users.noreply.github.com>
2023-08-16 20:29:22 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "khushi"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" ==> 33
+//"33abc" ==>NaN
+//true ==> 1; false ==> 0
+
+let isLoggedIn = "khushi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1==>true;  0==>false
+// "" ==> false
+// "khushi" ==> true
+
+let someNumber = 33
+
+let stringNumner = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
